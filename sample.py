3d0cdf6ff0769ed4b7dd19fcf60568bd61d313e7
Anthony Brown <anthony0857@gmail.com>
2022-07-05 09:04:41 -0500

    refactor sample.py for more convenient use in colab

--- sample.py
+++ sample.py
@@ -20,13 +20,14 @@ from dalle_pytorch import DiscreteVAE, VQGanVAE
 
 from einops import rearrange
 from math import log2, sqrt
+from utils import *
 
 import argparse
 import pickle
 
 import os
 
-from encoders.modules import BERTEmbedder
+from encoders.modules import BERTEmbedder, MakeCutouts
 
 import clip
 
@@ -149,128 +150,101 @@ if args.ui_test:
     sys.exit()
 
 
-def fetch(url_or_path):
-    if str(url_or_path).startswith('http://') or str(url_or_path).startswith('https://'):
-        r = requests.get(url_or_path)
-        r.raise_for_status()
-        fd = io.BytesIO()
-        fd.write(r.content)
-        fd.seek(0)
-        return fd
-    return open(url_or_path, 'rb')
-
-
-class MakeCutouts(nn.Module):
-    def __init__(self, cut_size, cutn, cut_pow=1.):
-        super().__init__()
-
-        self.cut_size = cut_size
-        self.cutn = cutn
-        self.cut_pow = cut_pow
-
-    def forward(self, input):
-        sideY, sideX = input.shape[2:4]
-        max_size = min(sideX, sideY)
-        min_size = min(sideX, sideY, self.cut_size)
-        cutouts = []
-        for _ in range(self.cutn):
-            size = int(torch.rand([])**self.cut_pow * (max_size - min_size) + min_size)
-            offsetx = torch.randint(0, sideX - size + 1, ())
-            offsety = torch.randint(0, sideY - size + 1, ())
-            cutout = input[:, :, offsety:offsety + size, offsetx:offsetx + size]
-            cutouts.append(F.adaptive_avg_pool2d(cutout, self.cut_size))
-        return torch.cat(cutouts)
-
-
-def spherical_dist_loss(x, y):
-    x = F.normalize(x, dim=-1)
-    y = F.normalize(y, dim=-1)
-    return (x - y).norm(dim=-1).div(2).arcsin().pow(2).mul(2)
-
-
-def tv_loss(input):
-    """L2 total variation loss, as in Mahendran et al."""
-    input = F.pad(input, (0, 1, 0, 1), 'replicate')
-    x_diff = input[..., :-1, 1:] - input[..., :-1, :-1]
-    y_diff = input[..., 1:, :-1] - input[..., :-1, :-1]
-    return (x_diff**2 + y_diff**2).mean([1, 2, 3])
-
 device = torch.device('cuda:0' if (torch.cuda.is_available() and not args.cpu) else 'cpu')
 print('Using device:', device)
 
-model_state_dict = torch.load(args.model_path, map_location='cpu')
+def loadModels(
+        model_path="inpainting.pt",
+        bert_path="bert.pt",
+        kl_path="kl-f8.pt",
+        steps=None,
+        clip_guidance=False,
+        cpu=False,
+        ddpm=False,
+        ddim=False):
+    model_state_dict = torch.load(args.model_path, map_location='cpu')
 
-model_params = {
-    'attention_resolutions': '32,16,8',
-    'class_cond': False,
-    'diffusion_steps': 1000,
-    'rescale_timesteps': True,
-    'timestep_respacing': '27',  # Modify this value to decrease the number of
-                                 # timesteps.
-    'image_size': 32,
-    'learn_sigma': False,
-    'noise_schedule': 'linear',
-    'num_channels': 320,
-    'num_heads': 8,
-    'num_res_blocks': 2,
-    'resblock_updown': False,
-    'use_fp16': False,
-    'use_scale_shift_norm': False,
-    'clip_embed_dim': 768 if 'clip_proj.weight' in model_state_dict else None,
-    'image_condition': True if model_state_dict['input_blocks.0.0.weight'].shape[1] == 8 else False,
-    'super_res_condition': True if 'external_block.0.0.weight' in model_state_dict else False,
-}
+    model_params = {
+        'attention_resolutions': '32,16,8',
+        'class_cond': False,
+        'diffusion_steps': 1000,
+        'rescale_timesteps': True,
+        'timestep_respacing': '27',  # Modify this value to decrease the number of
+                                     # timesteps.
+        'image_size': 32,
+        'learn_sigma': False,
+        'noise_schedule': 'linear',
+        'num_channels': 320,
+        'num_heads': 8,
+        'num_res_blocks': 2,
+        'resblock_updown': False,
+        'use_fp16': False,
+        'use_scale_shift_norm': False,
+        'clip_embed_dim': 768 if 'clip_proj.weight' in model_state_dict else None,
+        'image_condition': True if model_state_dict['input_blocks.0.0.weight'].shape[1] == 8 else False,
+        'super_res_condition': True if 'external_block.0.0.weight' in model_state_dict else False,
+    }
 
-if args.ddpm:
-    model_params['timestep_respacing'] = 1000
-if args.ddim:
-    if args.steps:
-        model_params['timestep_respacing'] = 'ddim'+str(args.steps)
+    if ddpm:
+        model_params['timestep_respacing'] = 1000
+    if ddim:
+        if steps:
+            model_params['timestep_respacing'] = 'ddim'+str(steps)
+        else:
+            model_params['timestep_respacing'] = 'ddim50'
+    elif steps:
+        model_params['timestep_respacing'] = str(steps)
+
+    model_config = model_and_diffusion_defaults()
+    model_config.update(model_params)
+
+    if cpu:
+        model_config['use_fp16'] = False
+
+    # Load models
+    model, diffusion = create_model_and_diffusion(**model_config)
+    model.load_state_dict(model_state_dict, strict=False)
+    model.requires_grad_(clip_guidance).eval().to(device)
+
+    if model_config['use_fp16']:
+        model.convert_to_fp16()
     else:
-        model_params['timestep_respacing'] = 'ddim50'
-elif args.steps:
-    model_params['timestep_respacing'] = str(args.steps)
+        model.convert_to_fp32()
 
-model_config = model_and_diffusion_defaults()
-model_config.update(model_params)
+    def set_requires_grad(model, value):
+        for param in model.parameters():
+            param.requires_grad = value
 
-if args.cpu:
-    model_config['use_fp16'] = False
+    # vae
+    ldm = torch.load(kl_path, map_location="cpu")
+    ldm.to(device)
+    ldm.eval()
+    ldm.requires_grad_(clip_guidance)
+    set_requires_grad(ldm, clip_guidance)
 
-# Load models
-model, diffusion = create_model_and_diffusion(**model_config)
-model.load_state_dict(model_state_dict, strict=False)
-model.requires_grad_(args.clip_guidance).eval().to(device)
+    bert = BERTEmbedder(1280, 32)
+    sd = torch.load(bert_path, map_location="cpu")
+    bert.load_state_dict(sd)
 
-if model_config['use_fp16']:
-    model.convert_to_fp16()
-else:
-    model.convert_to_fp32()
+    bert.to(device)
+    bert.half().eval()
+    set_requires_grad(bert, False)
 
-def set_requires_grad(model, value):
-    for param in model.parameters():
-        param.requires_grad = value
+    # clip
+    clip_model, clip_preprocess = clip.load('ViT-L/14', device=device, jit=False)
+    clip_model.eval().requires_grad_(False)
+    normalize = transforms.Normalize(mean=[0.48145466, 0.4578275, 0.40821073], std=[0.26862954, 0.26130258, 0.27577711])
+    return model_params, model, diffusion, ldm, bert, clip_model, clip_preprocess, normalize
 
-# vae
-ldm = torch.load(args.kl_path, map_location="cpu")
-ldm.to(device)
-ldm.eval()
-ldm.requires_grad_(args.clip_guidance)
-set_requires_grad(ldm, args.clip_guidance)
-
-bert = BERTEmbedder(1280, 32)
-sd = torch.load(args.bert_path, map_location="cpu")
-bert.load_state_dict(sd)
-
-bert.to(device)
-bert.half().eval()
-set_requires_grad(bert, False)
-
-# clip
-clip_model, clip_preprocess = clip.load('ViT-L/14', device=device, jit=False)
-clip_model.eval().requires_grad_(False)
-normalize = transforms.Normalize(mean=[0.48145466, 0.4578275, 0.40821073], std=[0.26862954, 0.26130258, 0.27577711])
-
+modelParams, model, diffusion, ldm, bert, clip_model, clip_preprocess, normalize= loadModels(
+        model_path=args.model_path,
+        bert_path=args.bert_path,
+        kl_path=args.kl_path,
+        steps = args.steps,
+        clip_guidance = args.clip_guidance,
+        cpu = args.cpu,
+        ddpm = args.ddpm,
+        ddim = args.ddim)
 print("Loaded models")
 
 def createSampleFunction(image, mask, prompt, batch_size):
